412748dada16a2acb280f6e7526807639cb16f08
Arnav Thakare <162623054+errorbegone@users.noreply.github.com>
2024-09-05 01:09:16 +0530

    Update and rename datediff.sql to date_add.sql

--- datediff.sql
+++
@@ -1,3 +0,0 @@
--- link: https://leetcode.com/problems/rising-temperature/?envType=study-plan-v2&envId=top-sql-50 --
-select id from Weather w1, Weather w2 where datediff(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
-
